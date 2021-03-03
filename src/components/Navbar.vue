3c7f7c5d1a51626e572454f6c101fd06aa5e3e15
BrVenturaU <brandonventura16@gmail.com>
2021-03-03 15:24:29 -0600

    Resuelve problema de recarga de home

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,7 +7,7 @@
 
             <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav>
-                <b-nav-item :to="{name:'Home'}">Inicio</b-nav-item>
+                <b-nav-item :to="{name:'Dashboard'}">Inicio</b-nav-item>
             </b-navbar-nav>
 
             <!-- Right aligned nav items -->
