516dd31894af8a8d34f41afb7e7b06564a17a271
BrVenturaU <brandonventura16@gmail.com>
2021-03-03 14:20:00 -0600

    Crea barra de navegación

---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="primary">
+            <b-navbar-brand href="#">TodoList</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item :to="{}">Link #1</b-nav-item>
+                <b-nav-item :to="{}">Link #2</b-nav-item>
+            </b-navbar-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+                <b-nav-form>
+                    <b-form-input size="sm" class="mr-sm-2" placeholder="Buscar"></b-form-input>
+                    <b-button size="sm" class="my-2 my-sm-0" type="submit">Buscar</b-button>
+                </b-nav-form>
+                <b-nav-item-dropdown right>
+                <!-- Using 'button-content' slot -->
+                <template #button-content>
+                    <em>Usuario</em>
+                </template>
+                <b-dropdown-item href="#">Perfil</b-dropdown-item>
+                <b-dropdown-item href="#">Cerrar sesión</b-dropdown-item>
+                </b-nav-item-dropdown>
+            </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style scoped>
+
+</style>
