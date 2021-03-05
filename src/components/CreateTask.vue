cd36a3a5091452be294888222ce9711740a11d23
@vasga-floze <gabriela.floze@gmail.com>
2021-03-04 23:51:53 -0600

    agrega diseño para creacion de tareas

--- src/components/CreateTask.vue
+++ src/components/CreateTask.vue
@@ -1,10 +1,50 @@
 <template>
     <div class="container">
-        <div class="row">
-            <b-col cols="12">
-                <h1>Crear tarea</h1>
-            </b-col>
-        </div>
+        <section>
+
+          <b-card-group>
+
+              <b-card title="CREAR TAREA">
+                <b-form>
+                  <b-row class="my-1">
+                      <b-col sm="2">
+                        <label>Titulo tarea:</label>
+                      </b-col>
+                      <b-col sm="10">
+                        <b-form-input placeholder="Titulo de la tarea"></b-form-input>
+                      </b-col>
+                  </b-row>
+                  <b-row class="my-1">
+                      <b-col sm="2">
+                        <label>Descripción:</label>
+                      </b-col>
+                      <b-col sm="10">
+                        <b-form-textarea placeholder="Descripción"
+                        rows="3" max-rows="6"></b-form-textarea>
+                      </b-col>
+                  </b-row>
+                  <b-row class="my-1"> 
+                    <b-col sm="2">
+                          <label>Estado: </label>
+                    </b-col>              
+                    <b-col sm="10">
+                          <b-form-radio-group
+                            class="mb-9"
+                            value-field="status"
+                            text-field="name"
+                          ></b-form-radio-group>
+                    </b-col>
+                  </b-row>
+                  <b-row>
+                    <b-col sm="10">
+                        <b-button type="button" variant="primary">Crear tarea</b-button>
+                      </b-col>
+                  </b-row>
+                </b-form>
+              </b-card>
+          </b-card-group>
+              
+      </section>
     </div>
 </template>
 
